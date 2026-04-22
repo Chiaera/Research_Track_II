89ff7af407d48a40cb31ae81ad54ae916a6fb9b8
Chiaera <chiara.masa@hotmail.it>
2026-04-23 01:19:57 +0200

    add rviz config file

--- src/robot_bringup/launch/move_robot.launch.py
+++ src/robot_bringup/launch/move_robot.launch.py
@@ -15,7 +15,7 @@ def generate_launch_description():
         launch_arguments={'rviz': 'false'}.items()
     )
 
-    rviz_config_file = os.path.join(get_package_share_directory('config'), 'start_configuration.rviz')    
+    rviz_config_file = os.path.join(get_package_share_directory('robot_bringup'), 'config', 'start_configuration.rviz')    
     rviz_node = Node(
         package='rviz2',
         executable='rviz2',
